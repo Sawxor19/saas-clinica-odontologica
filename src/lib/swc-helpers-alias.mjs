98d7a2a7fd7f93e448456bb44a28166de0d0139a
Victor Hugo Sanches <sawxor19@gmail.com>
2026-02-07 19:52:30 -0300

    Fix swc helpers alias export

--- src/lib/swc-helpers-alias.mjs
+++ src/lib/swc-helpers-alias.mjs
@@ -1,2 +1,2 @@
 export * from "@swc/helpers/esm/index.js";
-export { _apply_decorated_descriptor as applyDecoratedDescriptor } from "@swc/helpers/esm/_apply_decorated_descriptor.js";
+export { _ as applyDecoratedDescriptor } from "@swc/helpers/esm/_apply_decorated_descriptor.js";
